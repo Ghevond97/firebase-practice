19de8d003f55232bdb56ff30f1f2b066c23540ec
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-07-01 00:57:43 +0400

    firebase authentification done. Signup, Signin, Signout, PasswordReset, PasswordUpdate functions imported from firebase, Signup Component with basic inputs and validation created, higher order component of firebase context created to avoid wrapping components with firebase consumer every time.

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -1,22 +1,33 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
-  // Your web app's Firebase configuration
-  var config = {
-    apiKey: "AIzaSyC4yDB0rPIHVQt9nsgAZrSSdr09lHCcQyM",
-    authDomain: "new-task-project-60574.firebaseapp.com",
-    databaseURL: "https://new-task-project-60574.firebaseio.com",
-    projectId: "new-task-project-60574",
-    storageBucket: "",
-    messagingSenderId: "466812015839",
-    appId: "1:466812015839:web:1c6b6929fde4f000"
-  };
-  // Initialize Firebase
-  // firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var config = {
+  apiKey: 'AIzaSyC4yDB0rPIHVQt9nsgAZrSSdr09lHCcQyM',
+  authDomain: 'new-task-project-60574.firebaseapp.com',
+  databaseURL: 'https://new-task-project-60574.firebaseio.com',
+  projectId: 'new-task-project-60574',
+  storageBucket: '',
+  messagingSenderId: '466812015839',
+  appId: '1:466812015839:web:1c6b6929fde4f000',
+};
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-  class Firebase {
-    constructor() {
-      app.initializeApp(config);
-    }
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
   }
-  
-  export default Firebase;
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from '../src/Components/App';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import Firebase, { FirebaseContext } from './Components/Firebase';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
